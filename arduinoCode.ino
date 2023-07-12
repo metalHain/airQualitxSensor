2ceaec16b50b306ecef03be99558849c5c7bfece
metalHain <133499290+metalHain@users.noreply.github.com>
2023-07-12 14:41:33 +0200

    Update arduinoCode.ino

--- arduinoCode.ino
+++ arduinoCode.ino
@@ -1,58 +1,62 @@
+
+#include <Wire.h>
 #include <AirQuality.h>
-#include <time.h>
-#include <TimeAlarms.h>
 
 AirQuality aq(12);
-AlarmId id;
 
-void setup() { 
-  Serial.begin(9600);
-  //aq.set_RTC_time(23,7,9,1,18,41,0);
+void setup() {
+  // check in your settings that the right speed is selected
+  Serial.begin(115200);
+  // wait for serial connection from PC
+  // comment the following line if you'd like the output
+  // without waiting for the interface being ready
+  while(!Serial);
 
+  // wait until sensors are ready, > 1000 ms according to datasheet
   delay(1000);
+  
+  // start scd measurement in periodic mode, will update every 5 s
+  Wire.beginTransmission(SCD_ADDRESS);
+  Wire.write(0x21);
+  Wire.write(0xb1);
+  Wire.endTransmission();
 
-  aq.retrieve_RTC_time(currentTime);
-  setTime(currentTime[2], currentTime[1], currentTime[0], currentTime[4], currentTime[5], currentTime[6]);
+  // wait for first measurement to be finished
+  delay(5000);
 
-  //id = Alarm.timerRepeat(10, dataLogging);
-  Serial.println("Setup done");
+  Serial.println("setup done");
 }
 
 void loop() {
-  dataLogging();
-}
-
-void dataLogging()
-{
   aq.retrieve_RTC_time(currentTime);
 
-  /*
   sensorVals.co = aq.co_concentration();
-  sensorVals.co2 = aq.co2_concentration();
+  sensorVals.co2 = (int) aq.co2_concentration();
   sensorVals.temperature = aq.room_temperature();
   sensorVals.humidity = aq.room_humidity();
-  */
-
-  sensorVals.co = 1;
-  sensorVals.co2 = (int) aq.co2_concentration();
-  sensorVals.temperature = 1;
-  sensorVals.humidity = 1;
   sensorVals.batLevel = aq.bat_Voltage();
 
+  // convert double values to string so serial.print can handle it
   char buffer[200];
 
-  // hour min sec day month year temperature humidity co2 co batVoltage
+  char co_conc[9];
+  dtostrf(sensorVals.co, 4, 4, co_conc);
+  
   char batVoltage_string[9];
-  dtostrf(sensorVals.batLevel, 4, 3, batVoltage_string);
+  dtostrf(sensorVals.batLevel, 4, 2, batVoltage_string);
+
+  char temperature_string[8];
+  dtostrf(sensorVals.temperature, 4, 2, temperature_string);
+
+  char humidity_string[8];
+  dtostrf(sensorVals.humidity, 4, 2, humidity_string);
   
-  char co_conc_temp[12];
-  dtostrf(aq.co_concentration(), 4, 4, co_conc_temp);
-  
-  sprintf(buffer, "%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%s\t%s\n", currentTime[2], currentTime[1], currentTime[0], currentTime[4], currentTime[5], currentTime[6], sensorVals.temperature, sensorVals.humidity, sensorVals.co2, co_conc_temp, batVoltage_string);
+
+  sprintf(buffer, "%d\t%d\t%d\t%d\t%d\t%d\t%s\t%s\t%d\t%s\t%s\n", currentTime[2], currentTime[1], currentTime[0], currentTime[4], currentTime[5], currentTime[6], temperature_string, humidity_string, sensorVals.co2, co_conc, batVoltage_string);
   
   aq.add_values_to_hourly_buf(sensorVals);
   aq.addValuesToSDCard(buffer);
   Serial.println(buffer);
 
-  delay(2000); 
+  delay(5000); 
 }
